19c5a6ead5af59faab2566f328107f354cd58868
Vipul Yadav <vipulyadav0479@gmail.com>
2025-03-25 21:36:51 +0530

    Route fixing

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <><h1 className='text-3xl font-mona-sans justify-center flex items-center flex-column
+    '>Home Page</h1></>  )
+}
+
+export default page
